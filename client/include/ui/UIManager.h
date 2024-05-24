b239283fbdf518ff46778417881a31efc2574c93
Anh Le <aqle@ucsd.edu>
2024-05-23 21:37:45 -0700

    Add files imgui and no error so far
    - I have to copy file from the repo and have to modify the header include

--- client/include/ui/UIManager.h
+++ client/include/ui/UIManager.h
@@ -4,6 +4,10 @@
 #include <string>
 #include <memory>
 #include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include "SetupParser.h"
 
 namespace ui {
