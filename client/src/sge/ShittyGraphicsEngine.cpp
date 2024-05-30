242084e454ab841c33d5184312e204ffec4a8f34
Alanine42 <alansun2020@126.com>
2024-05-30 10:43:53 -0700

    windows size debug print -- plz pull and test (windows and mac)

--- client/src/sge/ShittyGraphicsEngine.cpp
+++ client/src/sge/ShittyGraphicsEngine.cpp
@@ -40,7 +40,10 @@ void sge::sgeInit()
     }
     #endif
 
+    std::printf("window fucking size %d, %d\n", sge::windowWidth, sge::windowHeight);
     glfwGetFramebufferSize(window, &sge::windowWidth, &sge::windowHeight);
+    std::printf("window fucking size %d, %d\n", sge::windowWidth, sge::windowHeight);  // why is it 2x on mac?
+
     glViewport(0, 0, sge::windowWidth, sge::windowHeight);
     glEnable(GL_DEPTH_TEST);   // Only render stuff closest to camera
     glEnable(GL_STENCIL_TEST); // TODO: is to allow for rendering outlines around objects later. (e.g. outline around egg or something)
